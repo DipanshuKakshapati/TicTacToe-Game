a74f34438f12b49948c96f9866b61ca9eb800fb9
Dipanshu Kakshapati <dipanshu.ksh10@gmail.com>
2022-08-18 10:47:29 +0545

    insert functions

--- game.c
+++ game.c
@@ -1,6 +1,75 @@
 #include <stdio.h>
 
-int fac(int n)
+void display_board(char game_board[])
 {
-    return 1;
+    int i;
+    for(i=0;i<9;i++)
+    {
+        printf("%c ",game_board[i]);
+     
+        if((i+1)%3==0)
+        {
+            printf("\n");
+        }
+    }
 }
+
+void display_turn(char *turnof)                 
+{
+    printf("%c\n",*turnof);
+}
+
+void insert(char game_board[], char *turnof)
+{
+    int position;
+
+    printf("your turn %c\n",*turnof);
+    printf("enter where you want to place your play: ");
+    scanf("%i", &position);
+    game_board[position-1]=*turnof;
+}
+
+void switch_turn(char *turnof)
+{
+    if(*turnof=='X')
+    {
+        *turnof='O';
+    }
+    else
+    {
+        *turnof='X';
+    }
+}
+
+int main(void)
+{
+    char turnof = 'X';
+    char game_board[9] = {'_','_','_','_','_','_','_','_','_'};
+
+    display_board(game_board);
+    printf("\n");
+    display_turn(&turnof);
+    insert(game_board, &turnof);
+
+    display_board(game_board);
+
+    printf("\n");
+    switch_turn(&turnof);
+    display_turn(&turnof);
+    insert(game_board, &turnof);
+    display_board(game_board);
+    printf("\n");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char word[100] = {"hello "}
+}
